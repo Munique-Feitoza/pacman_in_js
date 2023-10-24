39ee1721b553f74425b6282085b950900a445e11
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-10-24 17:34:01 -0300

    refactor: Criação do mapa

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,51 +1,29 @@
 const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 const textScore = document.querySelector('.score');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
-class Boundary {
-  static width = 40;
-  static height = 40;
-	constructor({ position }) {
-		this.position = position;
-		this.width = 40;
-		this.height = 40;
-	}
-
-	draw() {
-		c.fillStyle = '#c500ff';
-		c.fillRect(this.position.x, this.position.y, this.width, this.height);
-	}
-}
+const SIZE = 40;
+const height = SIZE;
+const width = SIZE;
 
 const map = [
-  ['-', '-', '-', '-', '-', '-', '-'],
-  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
-  ['-', ' ', '-', ' ', '-', ' ', '-'],
-  ['-', ' ', ' ', ' ', ' ', ' ', '-'],
-  ['-', '-', '-', '-', '-', '-', '-']
-  ]
+	['-', '-', '-', '-', '-', '-', '-'],
+	['-', ' ', ' ', ' ', ' ', ' ', '-'],
+	['-', ' ', '-', ' ', '-', ' ', '-'],
+	['-', ' ', ' ', ' ', ' ', ' ', '-'],
+	['-', '-', '-', '-', '-', '-', '-'],
+];
 
-const boundaries = [];
+function draw(x, y) {
+	ctx.fillStyle = '#c500ff';
+	ctx.fillRect(x * SIZE, y * SIZE, height, width);
+}
 
-map.forEach((row, i) => {
-  row.forEach((symbol, j) => {
-    switch (symbol) {
-      case '-':
-        boundaries.push(
-          new Boundary({
-            position: {
-              x: Boundary.width * j,
-              y: Boundary.height * i
-            }
-          })
-        )
-        break;
-    }
-  })
-})
-boundaries.forEach((boundary) => {
-	boundary.draw();
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+
+map.forEach((arrRow, posY) => {
+	arrRow.forEach((symbol, posX) => {
+		symbol === '-' && draw(posX, posY);
+	});
 });
