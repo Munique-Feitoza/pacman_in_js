ece18b7b430c314db2b5a88dcd44a06bc0d21a75
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-10-24 12:53:47 -0300

    Padronização de código e formatação

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,33 +6,33 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Boundary {
-  constructor({position}) {
-    this.position = position;
-    this.width = 40
-    this.height = 40
-  }
-  
-  draw() {
-    c.fillStyle = '#c500ff';
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
+	constructor({ position }) {
+		this.position = position;
+		this.width = 40;
+		this.height = 40;
+	}
+
+	draw() {
+		c.fillStyle = '#c500ff';
+		c.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
 }
+
 const boundaries = [
-  new Boundary({
-    position: {
-      x: 0,
-      y: 0
-    }
-  }),
-  new Boundary({
-    position: {
-      x: 41,
-      y: 0
-    }
-  })
- ];
+	new Boundary({
+		position: {
+			x: 0,
+			y: 0,
+		},
+	}),
+	new Boundary({
+		position: {
+			x: 41,
+			y: 0,
+		},
+	}),
+];
 
-
-boundaries.forEach(boundary => {
-  boundary.draw();
-})
+boundaries.forEach((boundary) => {
+	boundary.draw();
+});
